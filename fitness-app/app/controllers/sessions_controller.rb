c570f0412565c6d226f760d41f704b170fa55352
Michelle Brant <michelle.brant18@gmail.com>
2017-01-17 10:51:22 -0500

    parse data

--- fitness-app/app/controllers/sessions_controller.rb
+++ fitness-app/app/controllers/sessions_controller.rb
@@ -6,7 +6,19 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    render :text => auth_hash.inspect
+    @userdata = auth_hash
+    @height = @userdata[:extra][:raw_info][:height]
+    @pic = 'http://static.mapmyfitness.com' + @userdata[:extra][:raw_info][:_links][:image][0][:href]
+    @weight = @userdata[:extra][:raw_info][:weight]
+    @goal = @userdata[:extra][:raw_info][:goal_statement]
+    @newweight = @weight * 2.20462262185
+    @newweight2 = @newweight.floor
+    @newheight = @height * 3.2808
+    @newheight2 = @newheight.floor
+    @first = @userdata[:info][:first_name]
+    @last = @userdata[:info][:last_name]
+
+
   end
 
   def destroy
