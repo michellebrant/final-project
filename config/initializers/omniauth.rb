71fd1ed66a853bd1e323e70263a51cf042351eb6
Michelle Brant <michelle.brant18@gmail.com>
2017-01-22 16:29:23 -0500

    var

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  var UAKEY = process.env.UAKEY
+  var UASECRETKEY = process.env.UASECRETKEY
   provider :underarmour, UAKEY, UASECRETKEY
 end
 
